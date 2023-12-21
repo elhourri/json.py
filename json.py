49f8e90ca60294f4127c6e8568067c12f3f09dca
elhourri <146944553+elhourri@users.noreply.github.com>
2023-12-21 22:07:11 +0100

    Update json.py

--- json.py
+++ json.py
@@ -11,20 +11,19 @@ with open("data.json", "w") as json_file:
 
 try:
     with open("data.json", "r") as json_file:
-        loaded_data = json.load(json_file)
+        initial_data = json.load(json_file)
         print("Contenu du fichier initial:")
-        print(loaded_data)
+        print(initial_data)
 
-    loaded_data["language"] = "Python"
+    initial_data["language"] = "Python"
 
     with open("data.json", "w") as json_file:
-        json.dump(loaded_data, json_file, indent=2)
+        json.dump(initial_data, json_file, indent=2)
 
     with open("data.json", "r") as json_file:
-        modified_data = json.load(json_file)
+        new_data = json.load(json_file)
         print("\nContenu du fichier après modification:")
-        print(modified_data)
-
+        print(new_data)
 except FileNotFoundError:
     print("Le fichier n'a pas été trouvé.")
 except json.JSONDecodeError:
